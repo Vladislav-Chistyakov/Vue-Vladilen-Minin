4971cb8f6b52afc3076084c286d935c2c36dafff
Vladislav_Chistyakov <vladischis@gmail.com>
2022-10-27 01:06:52 +0500

    Resuming the course

--- app.js
+++ app.js
@@ -1,39 +1,16 @@
 const App = {
   data() {
     return {
-      title: "Список заметок",
-      placeholder: "Плейсхолдер",
-      inputValue: "",
-      notes: [],
-    }    
-  }, 
-  methods: {
-    clickBtn() {
-      if (this.inputValue !== '') {
-        this.notes.push(this.inputValue);
-        this.inputValue = "";
-      }
-    },
-    removeNote(index) {
-      console.log("ev", index);
-      this.notes.splice(index, 1);
-    },
-    toUpperCase(item) {
-      return item.toUpperCase();
-    },
-  },
-  computed: {
-    doubleCountComputed() {
-      return this.notes.length * 2;
+      title: 'Список заметок',
+      placeholderString: 'Введите текст',
+      inputValue: '',
     }
   },
-  watch: {
-    inputValue(value) {
-      if (value.length > 10) {
-        this.inputValue = '';
-      }
+  methods: {
+    changeInputHeandler(event) {
+      this.inputValue = event.target.value
     },
   }
 }
 
-Vue.createApp(App).mount("#app")
+Vue.createApp(App).mount('#app') 
