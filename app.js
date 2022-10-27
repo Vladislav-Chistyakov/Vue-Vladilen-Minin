80992d298bbc514713cfa0e0f62548a31ecb01dd
Vladislav_Chistyakov <vladischis@gmail.com>
2022-10-27 14:22:09 +0500

    The module is based on the basics of vue.js completed

--- app.js
+++ app.js
@@ -4,12 +4,37 @@ const App = {
       title: 'Список заметок',
       placeholderString: 'Введите текст',
       inputValue: '',
+      notes: [],
     }
   },
   methods: {
-    changeInputHeandler(event) {
-      this.inputValue = event.target.value
+    addNewNote() {
+      if (this.inputValue !== '') {
+        this.notes.push(this.inputValue)
+        this.inputValue = ''
+      }
     },
+    deleteNote(ind) {
+      this.notes.splice(ind, 1)
+    },
+    toUpperCase(item) {
+      return item.toUpperCase()
+    },
+
+  },
+  computed: {
+    doubleCount() {
+      console.log("Povtor...")
+      return this.notes.length * 2
+    }
+  },
+  watch: {
+    inputValue(value) {
+      if (value.length > 10) {
+        this.inputValue = ''
+      }
+      console.log('resultat: ', value)
+    }
   }
 }
 
